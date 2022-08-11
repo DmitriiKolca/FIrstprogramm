9538d96a7dc3b2a924aeeef1861cca0dc5c3e238
Dmitrii Kolca <leonidi4@list.ru>
2022-08-11 11:20:35 +0300

    first commit

---
+++ Больше_Меньше.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Добро пожаловать в игру 'Больше Меньше'");
+
+Random rnd = new Random();
+int numm = rnd.Next(0, 100);
+Console.WriteLine($"{numm}");
+
+Console.WriteLine("Я загадал число от 0 до 100. Попробуй отгадать...");
+int otvet = Convert.ToInt32(Console.ReadLine());
+
+while (otvet != numm)
+{
+    if (otvet < numm)
+        Console.WriteLine("Больше");
+    else
+        Console.WriteLine("Меньше");
+
+    Console.WriteLine("Попробуйте еще раз");
+    otvet = Convert.ToInt32(Console.ReadLine());
+};
+
+Console.WriteLine("Поздравляю вы отгадали!");
+
+
+
+
